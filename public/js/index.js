ed8d350df2a251ea18d6de3cc882e1ea129b949c
Dika <dika.frandita@gmail.com>
2020-08-26 17:02:57 +0700

    broadcast to all host

--- public/js/index.js
+++ public/js/index.js
@@ -1,22 +1,12 @@
 var socket = io();
     socket.on('connect', () => {
       console.log('connected to server')
-
-      socket.emit('createEmail', {
-        to:'dika@frandita.com',
-        text: 'Aku ganteng'
-      })
-
-      socket.emit('sendMessage', {
-        from:'dika ganteng',
-        body: 'Halo Dika ganteng'
-      })
     })
 
     socket.on('disconnect', () => {
       console.log('disconnected to server')
     })
 
-    socket.on('newEmail', (email) => {
-      console.log('New email', email)
+    socket.on('send', (message) => {
+      console.log('New message:', message)
     })
--- server/server.js
+++ server/server.js
@@ -2,31 +2,27 @@ const path = require('path')
 const express = require('express')
 const http = require('http')
 const socketIO = require('socket.io')
+const { create } = require('domain')
 const publicPath = path.join(__dirname,'../public')
 const port = process.env.PORT || 5000
 
 var app = express()
 var server = http.createServer(app)
-var socket = socketIO(server)
+var io = socketIO(server)
 
 
 app.use(express.static(publicPath))
 
-socket.on('connection', (socket) => {
+io.on('connection', (socket) => {
   console.log('New user connected')
 
-  socket.emit('newEmail', {
-    from: 'mamat@manja.com',
-    text: 'Halo mamat manja',
-    createdAt: Date()
-  })
-
-  socket.on('createEmail', (newEmail) => {
-    console.log(`new Email :${JSON.stringify (newEmail)}`)
-  })
-
-  socket.on('sendMessage', (message) => {
-    console.log(`new message: ${JSON.stringify(message)}`)
+  socket.on('createMessage', (message) => {
+    console.log(`createMessage: ${JSON.stringify(message)}`);
+    io.emit('send', {
+      from: message.from,
+      body: message.body,
+      createdAt: new Date().getTime()
+    })
   })
 
   socket.on('disconnect', () => {
