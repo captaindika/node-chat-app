268f63bfe141432edc052496e32c14967659680e
Dika <dika.frandita@gmail.com>
2020-08-31 01:39:28 +0700

    update

--- server/utils/message.js
+++ server/utils/message.js
@@ -2,7 +2,7 @@ var moment = require('moment')
 moment.locale('id')
 var generateMessage = (from, text, color) => {
   var date = new Date()
-  // convert time to GMT +7
+  // convert time to GMT +7 heroku server
   var convertDate = moment(date).add(7,'h').format('dddd, h:mm:ss a')
  return {
     from,
